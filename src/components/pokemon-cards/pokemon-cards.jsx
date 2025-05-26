d065346b3ad86d23646495841f045c8775c70c61
Jonathan Raxa <jrax@Mac.attlocal.net>
2025-05-26 14:04:57 -0500

    add pokemon stats

--- src/components/pokemon-cards/pokemon-cards.jsx
+++ src/components/pokemon-cards/pokemon-cards.jsx
@@ -1,4 +1,4 @@
-import { ButtonGroup,  Button, Card } from 'react-bootstrap';
+import { ButtonGroup,  Button, Card, ListGroup } from 'react-bootstrap';
 import { useGetPokemon } from '../../hooks/useGetPokemon';
 import './pokemon-cards.less';
 
@@ -6,19 +6,25 @@ import './pokemon-cards.less';
 export const PokemonCards = () => {
 
   const { allPokemon, page, setPage, total } = useGetPokemon({ limit: 20 });
-
-  
+    console.log('all: ', allPokemon);
     return (
     <div className="card-container">
         <div className="pokemon-container">
           {allPokemon.map((poke) => {
             return (
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '18rem' }}
+            >
                 <Card.Img variant="top" src={poke.img} />
                 <Card.Body>
                     <Card.Title>{poke.name}</Card.Title>
+                    <ListGroup className="list-group-flush">
+                    {poke.stats.map((value) => (
+                      <ListGroup.Item key={value.stat?.name}><strong>{value.stat?.name}</strong>: {value.base_stat}</ListGroup.Item>
+                    )
+                    )}
+
+                    </ListGroup>
                     <Card.Text>
-                    details here...
                     </Card.Text>
                 </Card.Body>
              </Card>
--- src/hooks/useGetPokemon.js
+++ src/hooks/useGetPokemon.js
@@ -8,38 +8,37 @@ export const useGetPokemon = ({ limit }) => {
     const [page, setPage] = useState(0);
     const [total, setTotal] = useState();
 
+    const fetchAllPokemon = useCallback(async () => {
+      try {
+        const params = new URLSearchParams({
+          limit,
+          offset: page,
+        });
+  
+        const res = await fetch(`${url}?${params.toString()}`);
+        const pokemon = await res.json();
+  
+        const results = await Promise.all(
+          pokemon.results.map(async ({ name, url }) => {
+            const result = await fetch(url);
+            const individual = await result.json();
+            return {
+              name,
+              img: individual.sprites?.back_default,
+              stats: individual.stats
+            };
+          })
+        );
+        setAllPokemon(results);
+        setTotal(pokemon.count);
+      } catch (err) {
+        console.error('Failed to fetch Pokémon:', err);
+      }
+    },[limit, page]);
+
     useEffect(() => {
-        const fetchAllPokemon = async () => {
-          try {
-            const params = new URLSearchParams({
-              limit,
-              offset: page,
-            });
-      
-            const res = await fetch(`${url}?${params.toString()}`);
-            const pokemon = await res.json();
-            console.log('Fetched list:', pokemon);
-      
-            const results = await Promise.all(
-              pokemon.results.map(async ({ name, url }) => {
-                const result = await fetch(url);
-                const individual = await result.json();
-                return {
-                  name,
-                  img: individual.sprites?.back_default,
-                };
-              })
-            );
-      
-            setAllPokemon(results);
-            setTotal(pokemon.count);
-          } catch (err) {
-            console.error('Failed to fetch Pokémon:', err);
-          }
-        };
-      
-        fetchAllPokemon();
-      }, [limit, page]);
+      fetchAllPokemon();
+    });
 
 
     return {
